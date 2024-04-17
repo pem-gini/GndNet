46719d9c7f901cea4015d9f99e5d43e7de4e82e3
Finn Gegenmantel <f.gegenmantel@pem.rwth-aachen.de>
2024-04-17 22:14:03 +0200

    Update data preparation for cluster

--- run_data_preparation_cluster.sh
+++ run_data_preparation_cluster.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/zsh
 
-#SBATCH --cpus-per-task=5
+#SBATCH --cpus-per-task=11
 #SBATCH --mem-per-cpu=500M
 #SBATCH --job-name=GND_NET_DATA_PREP
 #SBATCH --output=output.%J.txt
-#SBATCH --time=00:30:00
+#SBATCH --time=00:45:00
+
+export CONDA_ROOT=$HOME/miniconda3
+export PATH="$CONDA_ROOT/bin:$PATH"
+
+source activate gndnet
 
 cd dataset_utils/gnd_data_generator
-python3 semKitti_morph_data.py
+python3 semKitti_morph_data_camera.py
